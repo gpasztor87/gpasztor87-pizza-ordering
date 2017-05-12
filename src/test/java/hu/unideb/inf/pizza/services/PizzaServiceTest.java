ee3a108ca4bacdd9b56fa290ecf507d98f0ae94e
Pásztor Gábor <gpasztor87@gmail.com>
2017-05-12 21:12:50 +0200

    Testing Pizza and User services.

--- src/test/java/hu/unideb/inf/pizza/services/PizzaServiceTest.java
+++ src/test/java/hu/unideb/inf/pizza/services/PizzaServiceTest.java
@@ -1,11 +1,51 @@
 package hu.unideb.inf.pizza.services;
 
+import hu.unideb.inf.pizza.models.Pizza;
+import hu.unideb.inf.pizza.services.interfaces.PizzaServiceInterface;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class PizzaServiceTest {
 
-    @Before
-    public void setUp() {
+    private EntityManagerFactory entityManagerFactory;
 
+    private PizzaServiceInterface pizzaService;
+
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("test");
+        pizzaService = new PizzaService();
+    }
+
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void testGetAllPizza() {
+        // Given
+        this.createTestPizza();
+
+        // When
+        List<Pizza> pizzaList = pizzaService.getAllPizza();
+
+        // Then
+        Assert.assertEquals(1, pizzaList.size());
+    }
+
+    private Pizza createTestPizza() {
+        Pizza pizza = new Pizza();
+        pizza.setName("Margarita");
+        pizza.setPrice(1350);
+        pizza.setDescription("description");
+
+        return pizza;
     }
 }
--- src/test/java/hu/unideb/inf/pizza/services/UserServiceTest.java
+++ src/test/java/hu/unideb/inf/pizza/services/UserServiceTest.java
@@ -1,11 +1,99 @@
 package hu.unideb.inf.pizza.services;
 
+import hu.unideb.inf.pizza.dao.UserDao;
+import hu.unideb.inf.pizza.dao.interfaces.UserDaoInterface;
+import hu.unideb.inf.pizza.models.User;
+import hu.unideb.inf.pizza.services.interfaces.UserServiceInterface;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class UserServiceTest {
 
-    @Before
-    public void setUp() {
+    private EntityManagerFactory entityManagerFactory;
 
+    @Mock
+    private UserDaoInterface userDao;
+
+    @Mock
+    private UserServiceInterface userService;
+
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("test");
+        userDao = new UserDao();
+
+        userService = new UserService();
+    }
+
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void testCreateUser() {
+        // Given
+        User user = this.createTestUser();
+
+        // When
+        User savedUser = userService.createUser(user);
+
+        // Then
+        User expectedUser = userService.getUserById(savedUser.getId());
+
+        Assert.assertEquals(expectedUser.getEmail(), user.getEmail());
+    }
+
+    @Test
+    public void testGetUserByEmail() {
+        // Given
+        User user = this.createTestUser();
+
+        // When
+        userService.createUser(user);
+
+        // Then
+        Assert.assertEquals(userService.getUserByEmail(user.getEmail()).getId(), user.getId());
+    }
+
+    @Test
+    public void testUpdateUser() {
+        // Given
+        User user = this.createTestUser();
+
+        // When
+        userService.createUser(user);
+        user.setName("Teszt Elek");
+        user.setEmail("teszt.elek@example.org");
+        user.setPassword("123456");
+        user.setPhone("012345678");
+        user.setAddress("Budapest");
+        userService.updateUser(user);
+
+        // Then
+        User expectedUser = userService.getUserById(user.getId());
+
+        Assert.assertEquals(expectedUser.getName(), user.getName());
+        Assert.assertEquals(expectedUser.getEmail(), user.getEmail());
+        Assert.assertEquals(expectedUser.getPassword(), user.getPassword());
+        Assert.assertEquals(expectedUser.getPhone(), user.getPhone());
+        Assert.assertEquals(expectedUser.getAddress(), user.getAddress());
+    }
+
+    private User createTestUser() {
+        User user = new User();
+        user.setName("Gipsz Jakab");
+        user.setPhone("06201234567");
+        user.setAddress("Debrecen");
+        user.setEmail("gipsz.jakab@example.org");
+        user.setPassword("password");
+
+        return user;
     }
 }
